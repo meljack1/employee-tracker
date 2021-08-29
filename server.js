aa03da02bc6ae2cc595d00316441c356fb1ee0d9
Mel Jack <melissa_jack@hotmail.co.uk>
2021-08-29 16:20:00 +0100

    Add functionality to view all departments, roles or employees

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const Queries = require('./lib/queries.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,22 +13,56 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    user: 'root',
+    // Put your local password here
+    password: '',
+    database: 'employees_db',
   },
   console.log(`Connected to the database.`)
 );
 
-inquirer
+const queries = new Queries(db);
+
+function chooseAction() {
+  inquirer
     .prompt([
     {
-        type: 'list',
-        message: "What would you like to do?",
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-        name: 'action',
+      type: 'list',
+      message: "What would you like to do?",
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit"],
+      name: 'action',
+    },
+    {
+      type: 'input',
+      message: "What do you want to call the new department?",
+      when: (response) => response.action === "Add a department",
+      name: 'departmentName',
     }
   ])
-  .then((response) => {
+    .then((response) => {
+      switch (response.action) {
+        case "View all departments": 
+          queries.viewDepartments();
+          chooseAction();
+          break;
+        case "View all roles":
+          queries.viewRoles();
+          chooseAction();
+          break;
+        case "View all employees":
+          queries.viewEmployees();
+          chooseAction();
+          break;
+      }
+  });
+}
 
-  })
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+chooseAction();
